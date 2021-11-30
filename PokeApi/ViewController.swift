f95c1064410084a4bf0b97a3ec8ae9f82d50dc74
ykGoose <72850129+ykGoose@users.noreply.github.com>
2021-11-30 21:21:15 +0900

    Initial Commit

---
+++ PokeApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeApi
+//
+//  Created by Владимир Падусов on 30.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
